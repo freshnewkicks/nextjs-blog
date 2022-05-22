6df9d2963c81f8fe6b8d0fd249092bd63841d6be
freshnewkicks <tylerdukedev@gmail.com>
2022-05-22 10:03:59 -0400

    First commit

--- pages/contact.js
+++ pages/contact.js
@@ -1,18 +1,3 @@
-import { Fragment } from 'react';
-import Head from 'next/head';
+export default function ContactPage() {
 
-import ContactForm from '../components/contact/contact-form';
-
-function ContactPage() {
-  return (
-    <Fragment>
-      <Head>
-        <title>Contact Me</title>
-        <meta name='description' content='Send me your messages!' />
-      </Head>
-      <ContactForm />
-    </Fragment>
-  );
 }
-
-export default ContactPage;
